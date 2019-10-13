9336ec35403e70a2367fe5e1db03a0eb68dc0541
Nikita Melnikov <nikmel2803@gmail.com>
2019-10-13 15:22:39 +0300

    change folder structure and add webpack

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './libs/other-libs/bootstrap-grid.css'
+import './libs/other-libs/reset.css'
+
+import './js/base'
+import './js/goda-page'
+import './js/history-page'
+import './js/home-page'
+import './js/victims-page'
+
+import './styles/link.scss'
+import './styles/goda-page.scss'
+import './styles/history-page.scss'
+import './styles/home-page.scss'
+import './styles/victims-page.scss'
