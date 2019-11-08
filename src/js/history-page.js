04b0e6ff8067cd03b0fb5c97cf30767c8ccfb24d
Nikita Melnikov <nikmel2803@gmail.com>
2019-11-08 16:02:44 +0300

    refactor history-page code

--- src/js/history-page.js
+++ src/js/history-page.js
@@ -1,64 +1,90 @@
-var blocksCount = $('.history-page__sliders-wrap > div').length;
-var currentSlide = 1;
-var isNotIdle = true;
+const slidesCount = $('.history-page__slide').length;
+console.log(slidesCount)
+let currentSlide = 1;
+let isNotIdle = true;
 
-$(window).bind('mousewheel', function (event) {
-  if (event.originalEvent.wheelDelta <= 0 && isNotIdle) {
-    isNotIdle = false;
-    var windowHeight = $(window).height();
-    if (currentSlide <= 4 && currentSlide >= 1) {
-      $(".history__text_slide").animate({top: "55%",}, 200, function () {
-      });
-      $(".history__text_slide").animate({top: "50%",}, 700, function () {
-      });
+function debounce(f, ms) {
 
-      $(".history-page__slide > img").animate({top: "60%",}, 200, function () {
-      });
-      $(".history-page__slide > img").animate({top: "50%",}, 1000, function () {
-      });
+  let isCooldown = false;
+
+  return function () {
+    if (isCooldown) return;
+
+    f.apply(this, arguments);
+
+    isCooldown = true;
+
+    setTimeout(() => isCooldown = false, ms);
+  };
+}
+
+/**
+ *
+ * @param {MouseWheelEvent} event
+ */
+function changeSlideOnMouseWheel(event) {
+  if (!isNotIdle) return;
+
+  const windowHeight = $(window).height();
+  const isScrollingDown = event.originalEvent.wheelDelta <= 0;
+
+  if (isScrollingDown) {
+    const canScrollDown = currentSlide >= 1 && currentSlide <= slidesCount - 1;
+    if (canScrollDown) {
+      isNotIdle = false;
+      // $(".history__text_slide").animate({top: "55%",}, 200, function () {
+      // });
+      // $(".history__text_slide").animate({top: "50%",}, 700, function () {
+      // });
+      //
+      // $(".history-page__slide > img").animate({top: "60%",}, 200, function () {
+      // });
+      // $(".history-page__slide > img").animate({top: "50%",}, 1000, function () {
+      // });
 
       currentSlide++;
       $(window).unbind('click');
       console.log(windowHeight)
-      $(".history-page__sliders-wrap").animate({
-        top: "-=" + windowHeight + "",
-      }, 700, function () {
-        isNotIdle = true;
-      });
-    } else {
+
+      $(".history-page__sliders-wrap").css('transform', `translateY(-${windowHeight * currentSlide - 1}px)`)
       isNotIdle = true;
+      // $(".history-page__sliders-wrap").animate({
+      //   top: "-=" + windowHeight + "",
+      // }, 700, function () {
+      //   isNotIdle = true;
+      // });
     }
 
-  } else if (event.originalEvent.wheelDelta >= 0 && isNotIdle) {
+  } else {
 
-    isNotIdle = false;
-    var windowHeight = $(window).height();
-    console.log(windowHeight)
-    if (currentSlide <= 5 && currentSlide >= 2) {
-      $(".history__text_slide").animate({top: "45%",}, 200, function () {
-      });
-      $(".history__text_slide").animate({top: "50%",}, 700, function () {
-      });
+    const canScrollUp = currentSlide >= 2 && currentSlide <= slidesCount;
+    if (canScrollUp) {
+      isNotIdle = false;
 
-      $(".history-page__slide > img").animate({top: "40%",}, 200, function () {
-      });
-      $(".history-page__slide > img").animate({top: "50%",}, 1000, function () {
-      });
+      // $(".history__text_slide").animate({top: "45%",}, 200, function () {
+      // });
+      // $(".history__text_slide").animate({top: "50%",}, 700, function () {
+      // });
+      //
+      // $(".history-page__slide > img").animate({top: "40%",}, 200, function () {
+      // });
+      // $(".history-page__slide > img").animate({top: "50%",}, 1000, function () {
+      // });
 
       currentSlide--;
-      $(".history-page__sliders-wrap").animate({
-        top: "+=" + windowHeight + "",
-      }, 700, function () {
-        isNotIdle = true;
-      });
-    } else {
-      isNotIdle = true;
+      // $(".history-page__sliders-wrap").animate({
+      //   top: "+=" + windowHeight + "",
+      // }, 700, function () {
+      //   isNotIdle = true;
+      // });
     }
   }
 
   $(".history-page__year-item").removeClass('history-page__year-item--active');
   $(".history-page__year-item[id='" + currentSlide + "']").addClass('history-page__year-item--active');
-});
+}
+
+$(window).bind('mousewheel', debounce(changeSlideOnMouseWheel, 500));
 
 
 // function fillP(forNumber) {
@@ -100,3 +126,11 @@ $(window).bind('mousewheel', function (event) {
 //     j++;
 //   }
 // });
+
+function nextSlide() {
+
+}
+
+function previousSlide() {
+
+}
