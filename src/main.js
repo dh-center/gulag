11050b1cb4b5930bbb3a5ddf8187bf821d06b2aa
Nikita Melnikov <nikmel2803@gmail.com>
2019-11-08 12:24:23 +0300

    remove unused files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-import './libs/other-libs/bootstrap-grid.css'
-import './libs/other-libs/reset.css'
-
 import './js/years-page'
 import './js/history-page'
 import './js/victims-page'
 
-import './styles/link.scss'
 import './styles/periods-page.scss'
 import './styles/history-page.scss'
 import './styles/home-page.scss'
