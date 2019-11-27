bc1b500c1f1d248b436fa2c5cef1c381d7e9ebc3
Nikita Melnikov <nikmel2803@gmail.com>
2019-11-27 19:26:23 +0300

    animation prototype

--- src/js/deportation-page.js
+++ src/js/deportation-page.js
@@ -1,8 +1,12 @@
 import debounce from "./debounce";
+import wait from "./wait";
 
 const slidesCount = $('.deportation-page__slide').length;
 let currentSlide = 1;
 const windowHeight = $(window).height();
+
+const mapDocument = $(".deportation-map__document");
+
 
 /**
  * Changes slide on scroll
@@ -21,12 +25,31 @@ function changeSlideOnMouseWheel(event) {
 /**
  * Goes to next slide
  */
-function nextSlide() {
+async function nextSlide() {
   const canGoNext = currentSlide >= 1 && currentSlide <= slidesCount - 1;
   if (canGoNext) {
     currentSlide++;
+
     if(currentSlide === 2) {
-      $("deportation-map__document").toggleClass('deportation-map__document--hidden');
+      $(".deportation-page__sliders-wrap").css('transform', `translateY(-${windowHeight * (currentSlide - 1)}px)`);
+
+      await wait(500);
+      mapDocument.removeClass('deportation-map__document--not-visible');
+
+      mapDocument.removeClass('deportation-map__document--hidden-top');
+      mapDocument.addClass('deportation-map__document--normal');
+    }
+
+    if (currentSlide === 3) {
+      mapDocument.removeClass('deportation-map__document--normal');
+      mapDocument.addClass('deportation-map__document--hidden-bottom');
+
+      await wait(1000);
+      // mapDocument.css('visibility', 'hidden');
+      mapDocument.addClass('deportation-map__document--not-visible');
+      $(".deportation-page__sliders-wrap").css('transform', `translateY(-${windowHeight * (currentSlide - 1)}px)`);
+
+      return;
     }
 
     $(".deportation-page__sliders-wrap").css('transform', `translateY(-${windowHeight * (currentSlide - 1)}px)`);
