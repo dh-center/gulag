dea1265d6d295ba15a158f209218b525863ee4e2
Ilya Moroz <37909603+ilyamore88@users.noreply.github.com>
2019-12-02 00:05:05 +0300

    Translate deportation page (#35)
    * translate texts
    
    * translate images

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,15 @@ const contentDataset = {
     periodsInfo: require('./src/content/ru/periodsInfo'),
     dict: require('./src/content/ru/dict'),
     history: require('./src/content/ru/history'),
-    victims: require('./src/content/ru/victims')
+    victims: require('./src/content/ru/victims'),
+    deportations: require('./src/content/ru/deportations')
   },
   en: {
     periodsInfo: require('./src/content/en/periodsInfo'),
     dict: require('./src/content/en/dict'),
     history: require('./src/content/en/history'),
-    victims: require('./src/content/en/victims')
+    victims: require('./src/content/en/victims'),
+    deportations: require('./src/content/en/deportations')
   }
 };
 
